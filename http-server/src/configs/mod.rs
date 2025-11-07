834fe6db32a651568847ac97f6d3ba3ccfe0db28
Anton Iskryzhytskyi <anton.i@affinidi.com>
2025-11-07 11:41:26 +0200

    feat: expose profile dids via http-server (#15)

--- http-server/src/configs/mod.rs
+++ http-server/src/configs/mod.rs
@@ -1,13 +1,21 @@
 use app::configs::{Configs, TrustStorageBackend};
+use serde::{Deserialize, Serialize};
 use std::env;
+use tracing::warn;
 
 const DEFAULT_LISTEN_ADDRESS: &str = "0.0.0.0:3232";
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ProfileConfig {
+    pub did: String,
+}
 
 #[derive(Debug, Clone)]
 pub struct HttpServerConfigs {
     pub(crate) listen_address: String,
     pub(crate) storage_backend: TrustStorageBackend,
     pub(crate) cors_allowed_origins: Vec<String>,
+    pub(crate) profile_configs: Vec<ProfileConfig>,
 }
 
 impl Configs for HttpServerConfigs {
@@ -32,10 +40,25 @@ impl Configs for HttpServerConfigs {
             .filter(|s| !s.is_empty())
             .collect();
 
+        let profile_configs = match env::var("PROFILE_CONFIGS") {
+            Ok(s) => match serde_json::from_str::<Vec<ProfileConfig>>(&s) {
+                Ok(cfgs) => cfgs,
+                Err(e) => {
+                    warn!("Failed to parse PROFILE_CONFIGS as JSON: {e}");
+                    Vec::new()
+                }
+            },
+            Err(_) => {
+                warn!("Env var PROFILE_CONFIGS is not provided. The list will be empty");
+                Vec::new()
+            }
+        };
+
         Ok(HttpServerConfigs {
             listen_address,
             storage_backend,
             cors_allowed_origins,
+            profile_configs,
         })
     }
 }
