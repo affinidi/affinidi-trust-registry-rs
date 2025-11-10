3e2d112d2ddf92d396e6dd684e4f9ee495d1e390
YoussefAWasfy <youssef.w@affinidi.com>
2025-11-10 08:59:15 +0100

    fix: redundant message processing

--- didcomm-server/src/listener/start_listener.rs
+++ didcomm-server/src/listener/start_listener.rs
@@ -3,19 +3,26 @@ use tracing::{debug, error};
 use crate::listener::*;
 
 impl<H: MessageHandler> Listener<H> {
-    pub async fn start_listening(&self) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        self.set_public_acls_mode().await?;
-        loop {
-            // TODO: review implementation of offline messages processing
-            // as it causes redundant processing of messages
-            let offline_messages_result = self.sync_and_process_offline_messages().await;
-            if let Err(e) = offline_messages_result {
-                error!(
-                    "[profile = {}] Error returned from offline_messages_result function. {}",
-                    &self.profile.inner.alias, e
-                );
-            }
+    pub async fn start_listening(
+        self: Arc<Self>,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        self.clone().set_public_acls_mode().await?;
+        let cloned_self = self.clone();
+        tokio::spawn(async move {
+            loop {
+                tokio::time::sleep(std::time::Duration::from_secs(30)).await;
+                let offline_messages_result = cloned_self.sync_and_process_offline_messages().await;
 
+                if let Err(e) = offline_messages_result {
+                    error!(
+                        "[profile = {}] Error returned from offline_messages_result function. {}",
+                        &cloned_self.profile.inner.alias, e
+                    );
+                }
+            }
+        });
+
+        loop {
             let next_message_result = self.process_next_message().await;
 
             if let Err(e) = next_message_result {
