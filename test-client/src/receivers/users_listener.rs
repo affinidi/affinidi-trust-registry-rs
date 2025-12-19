421ad555ca66ee5050f8eb51e5b9ba927d611bd0
oleksii <oleksii.p@affinidi.com>
2025-12-19 13:34:22 +0200

    fix: fix admin operations in test-client and implement listener termination after consecutive empty responses

--- test-client/src/receivers/users_listener.rs
+++ test-client/src/receivers/users_listener.rs
@@ -12,6 +12,10 @@ pub async fn user_listener(
     // if !to_delete.is_empty() {
     //     delete_messages_received(atm, &protocols, service_profile, &to_delete).await;
     // }
+
+    let mut empty_response_count = 0;
+    let max_empty_responses = 3;
+
     loop {
         println!("[{}] waiting for messages", did_config.alias);
         match protocols
@@ -22,13 +26,25 @@ pub async fn user_listener(
             Ok(msg) => {
                 if let Some(message) = msg {
                     println!("[{:?}] - Response: {:#?}", did_config.alias, message.0);
+                    empty_response_count = 0;
+                } else {
+                    empty_response_count += 1;
+                    if empty_response_count >= max_empty_responses {
+                        println!("[{}] No more messages received. Terminating listener.", did_config.alias);
+                        break;
+                    }
                 }
             }
             Err(err) => {
                 println!(
                     "Error in receiving message for {}: {:#?}",
                     did_config.alias, err
-                )
+                );
+                empty_response_count += 1;
+                if empty_response_count >= max_empty_responses {
+                    println!("[{}] Multiple errors or timeouts. Terminating listener.", did_config.alias);
+                    break;
+                }
             }
         };
     }
